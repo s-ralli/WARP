1fbd3bb6b1e2cff330dbeb15dd9f28370c8d30c0
s-ralli <128655039+s-ralli@users.noreply.github.com>
2023-06-01 10:35:00 -0700

    Update settings.py

--- bin/settings.py
+++ bin/settings.py
@@ -1,7 +1,7 @@
 import os
 
 __version__ = "v0.0.1"   
-__program__ = "lc_fam"
-HOME_VAR = "LC_FAM_HOME"
-RESOURCES_VAR = "LC_FAM_RESOURCES"
+__program__ = "WARP"
+HOME_VAR = "WARP_FAM_HOME"
+RESOURCES_VAR = "WARP_FAM_RESOURCES"
 MAX_JOBS = 20
